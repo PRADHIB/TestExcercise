cae7a02688ed119bdd3cf8f4a1fa2830dc1e335a
Babu <47215094+PRADHIB@users.noreply.github.com>
2019-02-01 11:36:49 +0530

    Add files via upload

---
+++ FileData/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+using System.Configuration;
+
+namespace FileData
+{
+    public static class Program
+    {
+
+        private static string ReadConfigKeys(string Key)
+        {
+            return ConfigurationSettings.AppSettings.Get(Key).ToString();
+        }
+
+        public static string ReturnFileInformation(string fileOption, string filePath)
+        {
+            string fileInformation = "";
+            FileDetails fileDetails = new FileDetails();
+            var fileSize = Program.ReadConfigKeys("size");
+
+            if (fileSize.Split(',').Contains(fileOption))
+            {
+                fileInformation = fileDetails.Size(filePath).ToString();
+
+            }
+            else
+            {
+                var fileVersion = Program.ReadConfigKeys("version");
+                if (fileVersion.Split(',').Contains(fileOption))
+                {
+                    fileInformation = fileDetails.Version(filePath).ToString();
+                }
+            }
+
+            return fileInformation;
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string fileInfo = Program.ReturnFileInformation(args[0], args[1]);
+                Console.WriteLine(fileInfo);
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+        }
+
+
+
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6d5c702-71ff-4743-9b22-cd75815e6c4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
